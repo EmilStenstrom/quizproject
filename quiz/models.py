9ae0fa23f5eed4b21fbd33124ee38b24a2d3be39
Emil Stenström <em@kth.se>
2017-09-24 14:20:27 +0200

    Upgrade to Django 2.0a1.

--- quiz/models.py
+++ quiz/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     answer2 = models.CharField(max_length=100)
     answer3 = models.CharField(max_length=100)
     correct = models.PositiveIntegerField()
-    quiz = models.ForeignKey(Quiz, related_name="questions")
+    quiz = models.ForeignKey(Quiz, related_name="questions", on_delete=models.CASCADE)
 
     def __str__(self):
     	return self.quiz.name + " / " + self.question
